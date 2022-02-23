fb2ba024ea8f38292b42118812eb4d7033fe7fa6
Brinelson <ladofmusic@gmail.com>
2022-02-23 12:23:12 -0700

    update fixes for dev not working

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,25 +1,25 @@
-// import {getMovieByTitle} from "../../utils/api-utils"
-// import { Box, Card, CardMedia, CardContent, Typography, CardActions } from '@mui/material'
-// const MovieList = (props) => {
-//     return (
-//         <>
-//             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-//                 <p>movie data goes here</p>
-// {/* omdb key  */}
-//             </Box>
-//             </>
-//     )
-// }
+import {getMovieByTitle} from "../../utils/api-utils"
+import { Box } from '@mui/material'
+const MovieList = (props) => {
+    return (
+        <>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <p>movie data goes here</p>
+{/* omdb key  */}
+            </Box>
+            </>
+    )
+}
 
-// export async function getStaticProps() {
+export async function getStaticProps() {
     
-//     const fetchedMovies = await getMovieByTitle('Thor')
+    const fetchedMovies = await getMovieByTitle('Thor')
 
-//     return {
-//         props: { 
-//             movie: fetchedMovies
-//         }
-//     }
-// }
+    return {
+        props: { 
+            movie: fetchedMovies
+        }
+    }
+}
 
-// export default MovieList
+export default MovieList
