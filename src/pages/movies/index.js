c8ec50f5b5559aeff4d5c50642f54f094007be4a
Brinelson <ladofmusic@gmail.com>
2022-02-27 00:00:36 -0700

    fixed a bunch of stuf with layout

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,27 +1,50 @@
-import {getMovieByTitle} from "../../utils/api-utils"
-import { Box } from '@mui/material'
+import { getMovieByTitle } from "../../utils/api-utils";
+import {
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+} from "@mui/material";
 const MovieList = (props) => {
-    const {movie} = props
-    return (
-        <>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <p>{movie.Title}</p>
-                <p></p>
+  const { movie } = props;
+  return (
+    <>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Card sx={{ maxWidth: 300 }}>
+                  <CardMedia component="img" title={movie.Title} image={movie.Poster} />
+                  <Box>
+          <CardContent>
+            <Typography variant="h5" color="initial">
+              {movie.Title}
+            </Typography>
+            <Typography variant="subtitle1" color="initial">
+              {movie.Rated}
+            </Typography>
+            <Typography variant="subtitle2" color="initial">
+              {movie.Director}
+            </Typography>
 
-            </Box>
-            </>
-    )
-}
+            <Typography variant="body1" color="initial">
+              {movie.Plot}
+            </Typography>
+                      </CardContent>
+                      </Box>
+        </Card>
+      </Box>
+    </>
+  );
+};
 
 export async function getStaticProps() {
-    
-    const fetchedMovies = await getMovieByTitle('Speed Racer')
+  const fetchedMovies = await getMovieByTitle("Speed Racer");
 
-    return {
-        props: { 
-            movie: fetchedMovies
-        }
-    }
+  return {
+    props: {
+      movie: fetchedMovies,
+    },
+  };
 }
 
-export default MovieList
+export default MovieList;
--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -1,6 +1,5 @@
 export async function getMovieByTitle(title) {
     const response = await fetch(`http://www.omdbapi.com/?apikey=17669979&t=${title}&plot=full`)
-    const data = await response.json()
-    console.log(data)
-    return data
+return await response.json()
+    
 }
