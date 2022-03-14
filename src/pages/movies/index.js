80143c63b0791b2d718d7d7e0ca48439c41c11ee
Brinelson <ladofmusic@gmail.com>
2022-03-14 03:55:32 -0600

    datastore delete code done

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,3 +1,7 @@
+import Amplify, { DataStore } from "aws-amplify"
+import useSWR from "swr"
+import {moviesDB} from "../../models"
+import config from "../../aws-exports"
 import { getMovieByTitle } from "../../utils/api-utils";
 import {
   Box,
@@ -7,8 +11,32 @@ import {
   Typography,
   CardActions,
 } from "@mui/material";
-const MovieList = (props) => {
-  const { movie } = props;
+
+Amplify.configure(config)
+const MovieList = () => {
+  const [movieList, setMovieList] = React.useState()
+  
+  const handleDelete = async (movie) => {
+    try {
+      const movieToDelete = await DataStore.query(moviesDB, movie.id)
+    
+      await DataStore.delete(movieToDelete)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const fetcher = async () => {
+    try {
+      let tempList = await DataStore.query(moviesDB)
+      setMovieList(tempList)
+    } catch (err){
+      console.log(err)
+    }
+    return movieList
+}
+
+  const {data, error} = useSWR('/movies', fetcher)
   return (
     <>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
@@ -37,20 +65,20 @@ const MovieList = (props) => {
   );
 };
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
  
  
  
  
  
  
-  // const fetchedMovies = await getMovieByTitle("Speed Racer");
+//   // const fetchedMovies = await getMovieByTitle("Speed Racer");
 
-  // return {
-  //   props: {
-  //     movie: fetchedMovies,
-  //   },
-  // };
-}
+//   // return {
+//   //   props: {
+//   //     movie: fetchedMovies,
+//   //   },
+//   // };
+// }
 
 export default MovieList;
