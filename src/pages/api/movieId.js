06dddbb44370f866d4d769face8be547d27a96ca
Brinelson <ladofmusic@gmail.com>
2022-04-28 23:10:22 -0600

    second api route

---
+++ src/pages/api/movieId.js
@@ -0,0 +1,13 @@
+// export default async function handler(req, res) {
+    
+//     if (req.method === 'POST') {
+        
+//         const title = req.body.title
+//         const response = await fetch(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=PuUQcHotZnAXXYNAbwGxPfwLVS81iC7tfeV3s9Wv&search_value=${title}&search_type=1'`)
+//         const movieData = await response.json()
+//         console.log(movieData)
+//         res.status(200).json(movieData)
+    
+// }
+
+// }
