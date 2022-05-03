d07d64faf22a77c2f49b2ab3291ecc2a5e2c7215
Brinelson <ladofmusic@gmail.com>
2022-05-02 21:28:20 -0600

    env fixed

--- src/pages/api/movie.js
+++ src/pages/api/movie.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
         
         const title = req.body.title
-        const response = await fetch(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=PuUQcHotZnAXXYNAbwGxPfwLVS81iC7tfeV3s9Wv&search_value=${title}&search_type=1'`)
+        const response = await fetch(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=${process.env.WATCHMODE_APIKEY}&search_value=${title}&search_type=1'`)
         const movieData = await response.json()
         
         res.status(200).json(movieData)
